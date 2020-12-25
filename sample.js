07403153e5cd02d5fa1ffbf192e97891b49fabdf
victory <vickywane@gmail.com>
2020-12-25 09:02:58 +0100

    Boilerplate

---
+++ sample.js
@@ -0,0 +1,81 @@
+const test = [
+  {
+    name: "White Burgundy",
+    category: "Dry white wine",
+    description: "A dry white wine which is bold and full bodied.",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129747/wine/white_burgundy-removebg-preview.png",
+  },
+  {
+    name: "Merlot",
+    category: "Dry red wine",
+    description:
+      "A smooth and medium bodied red wine originally from the Bordeaux region of France",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129747/wine/white_burgundy-removebg-preview.png",
+  },
+  {
+    name: "Moscato",
+    category: "Dessert wine",
+    description: "A vibrant and aromatic wine from the Italian dessert plains.",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129752/wine/moscato.png",
+  },
+  {
+    name: "Sparkling Wine",
+    category: "Rose wine",
+    description:
+      "A great wine with significant levels of carbon dioxide in it, making it fizzy upon consumption",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607130312/wine/rose-wine-removebg-preview.png",
+  },
+  {
+    name: "Cream sherry",
+    category: "Sherry wine",
+    description:
+      "A Spanish fortified wine made from white grapes that are grown near the city of Jerez de la Frontera in Andalusia, Spain",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129750/wine/cream_sherry-removebg-preview.png",
+  },
+  {
+    name: "Banyuls",
+    category: "Dessert wine",
+    description:
+      "A fortified dessert wine made from old vines cultivated in terraces on the slopes of the Catalan Pyrenees in the Roussillon county of France.",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129752/wine/banyuls.png",
+  },
+  {
+    name: "Madeira",
+    category: "Derset Wine",
+    description:
+      "A fortified dry wine originating from Madiera island of Portugal can be consumed alone as an aperitif.",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129744/wine/madiera-removebg-preview.png",
+  },
+  {
+    name: "Champange",
+    category: "Sparkling Wine",
+    description:
+      "A sparkling white wine made from a blend of grapes, especially Chardonnay and pinot, produced in Champagne by the méthode champenoise",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129751/wine/champange-removebg-preview.png",
+  },
+  {
+    name: "Pinotage",
+    category: "Red Wine",
+    description: "A red wine made with grapes from the South African regions",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607129748/wine/pinotage.png",
+  },
+  {
+    name: "Pinot Noir",
+    category: "Dry Red Wine",
+    description:
+      "A French red wine mostly made from Pinot Noir grapes which are grown in several regions around the world.",
+    image_uri:
+      "https://res.cloudinary.com/dkfptto8m/image/upload/v1607130312/wine/pinot-noir-removebg-preview.png",
+  },
+];
+
+export default test;
